7b582da832ede7a574c9487ced9a34891e88dbf7
Dorian <dubois.dorian2@gmail.com>
2024-02-06 19:22:34 +0100

    INIT

---
+++ app/src/main/java/fr/ensitech/dodoapp/tags/Tags.java
@@ -0,0 +1,9 @@
+package fr.ensitech.dodoapp.tags;
+
+import fr.ensitech.dodoapp.activities.MainActivity;
+import fr.ensitech.dodoapp.activities.SecondeActivity;
+
+public class Tags {
+    public static String MAIN_ACTIVITY = MainActivity.class.getSimpleName();
+    public static String SECONDE_ACTIVITY = SecondeActivity.class.getSimpleName();
+}
